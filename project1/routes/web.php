a56e928976eb0a12959afae801899445ff379dff
alekszak006 <alekszak06@icloud.com>
2023-09-27 08:29:05 +0200

    aa

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -29,3 +29,75 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// WAŻNE Musisz dopisać to co jest podspodem zamiast kopiowac!
+// Route jest od klasy
+// ::get([nazwa którą trzeba wpisac po/ żeby wyswietlilo strone], function(){[Co ma wyświetlić po wpisaniu linka]})
+Route::get('city_old',function(){
+return 'Miasto';
+
+
+});
+Route::get('city',function(){
+
+return view('city');
+
+});
+Route::get('info',function(){
+    return view('info',['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan']);
+});
+
+Route::get('info_age/{age}',function($age){
+    return view('info',['firstName' => 'Janusz', 'lastName' => 'Nowak', 'city' => 'Poznan','age' => $age]);
+});
+Route::get('pages/{page}',function($page){
+$pages = [
+    'about' => '2 słowa o mnie',
+    'contact' => 'contact@gmail.com',
+    'home' => 'Strona Domowa'
+];
+ return $pages[$page];
+
+});
+
+
+/*Route::get("/adress/{city}", function(string $city){
+    echo "Miasto: $city";
+});
+
+Route::get("/adress1/{city}/{street}", function(string $city, string $street){
+    echo <<< ADRESS
+    Miasto: $city<br>
+    Ulica: $street
+ADRESS;
+});
+Route::get("/adress2/{city}/{street}/{zipCode?}", function(string $city, string $street, string $zipCode =null
+){
+    if(is_null($zipCode))
+        $zipCode="brakdanych";
+    else{
+        $zipCode=substr($zipCode,0,2).'-'.substr($zipCode,2,5);
+    }
+    echo <<< ADRESS
+    Kod pocztowy: $zipCode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADRESS;
+});*/
+
+Route::get("/adress3/{city?}/{street?}/{zipCode?}", function(string $city="-", string $street="-", string $zipCode =null
+){
+    if(is_null($zipCode))
+        $zipCode="brakdanych";
+    else{
+        $zipCode=substr($zipCode,0,2).'-'.substr($zipCode,2,5);
+    }
+    echo <<< ADRESS
+    Kod pocztowy: $zipCode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADRESS;
+});
+
